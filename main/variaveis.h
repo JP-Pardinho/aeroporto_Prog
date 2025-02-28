9d5bd81dd49d71b15926055cac77bd8450b75f52
jp-pardinho <jppardinhor@gmail.com>
2025-02-28 14:00:25 -0300

    implementação do código de Fizon

---
+++ main/variaveis.h
@@ -0,0 +1,102 @@
+#ifndef VARIAVEIS_H
+#define VARIAVEIS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+
+#define FILAS 10
+#define COLUNAS 6
+
+// Structs
+typedef struct
+{
+    char codigo[4];
+    char nome[50];
+    char cidade[50];
+    char estado[3];
+}Aeroporto;
+
+typedef struct
+{
+    int codigo;
+    char nome[100];
+    char origem[4];
+    char destino[4];
+    char horarios[10][6];
+    int poltronas_total;
+    int poltronas_disponiveis;
+    float distancia;
+    char dias_semana[7];
+} Rota;
+
+typedef struct
+{
+    int codigo;
+    char nome[100];
+    char RG[20];
+    char CPF[12];
+    char data_nasc[11];
+    char telefone[15];
+    char email[50];
+    char contato_emergencia[100];
+    char bagagem_extra;
+} Passageiro;
+
+typedef struct
+{
+    int matricula;
+    char nome[100];
+    char cargo[50];
+} Funcionario;
+
+typedef struct
+{
+    int codigo_venda;
+    char cpf_passageiro[12];
+    int codigo_rota;
+    char origem[4];  // Adicionado
+    char destino[4]; // Adicionado
+    char horario[6];
+    char assento[4];
+    float valor_total;
+    char forma_pagamento[20];
+} Venda;
+
+// Protótipos de funções
+
+// Funções de alocação (COLOCAR OS PARAMETROS )
+Aeroporto* alocaVetAeroportos(int tam);
+Rota* alocaVetRotas(int tam);
+Passageiro* alocaVetPassageiros(int tam);
+Funcionario* alocaVetFuncionarios(int tam);
+Venda* alocaVetVenda(int tam);
+
+// Funções de Verificações
+void normalizaString();
+int verificaExistenciaAeroporto();
+int verificaExistenciaFuncionario();
+int verificaExistenciaCliente();
+int verificaCodigoAeroporto();
+int letras();
+int verificaNomePassageiro();
+int obterInteiro();
+
+
+// Protótipos de funções
+void exibir_menu_principal();
+void menu_configuracoes(Aeroporto *aeroportos, int *total_aeroportos, Rota *rotas, int *total_rotas, Passageiro *passageiros, int *total_passageiros, Funcionario *funcionarios, int *total_funcionarios);
+void menu_vendas(Rota *rotas, int total_rotas, Passageiro *passageiros, int total_passageiros, Venda *vendas, int *total_vendas);
+void cadastrar_aeroporto(Aeroporto *aeroportos, int *total);
+void cadastrar_rota(Rota *rotas, int *total);
+void salvar_arquivo(const char *nome_arquivo, void *dados, size_t tamanho_elemento, int total);
+int carregar_arquivo(const char *nome_arquivo, void *dados, size_t tamanho_elemento);
+float calcular_preco(Rota rota, int dias_antecedencia, char tipo_dia, float percentual_ocupacao, int dias_retorno);
+void gerar_eticket(Venda venda, Passageiro passageiro, Rota rota);
+
+
+
+
+#endif
